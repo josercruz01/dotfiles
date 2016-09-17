61f2d8a6ba8c423a84c4e45d42023ac8ac2f82b2
Jose Raymundo Cruz <jose.r.cruz01@gmail.com>
2016-09-17 13:54:02 -0400

    add gpg to cask and allow gitconfig to have a local file

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -12,9 +12,10 @@ apps=(
     spotify
     vlc
     licecap
+    gpgtools
 )
 
-brew cask install "${apps[@]}"
+brew cask install --require-sha "${apps[@]}"
 
 # Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
